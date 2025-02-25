0f085ebc38be95845249e389c4767cc736727af6
ah987 <ah987@students.waikato.ac.nz>
2025-02-25 17:16:25 +1300

    added mod description

--- ModHelperData.cs
+++ ModHelperData.cs
@@ -6,7 +6,7 @@ public static class ModHelperData
     public const string Version = "1.0.0";
     public const string Name = "TerrainSwap";
 
-    public const string Description = "An empty mod";
+    public const string Description = "Swaps water and land area types.";
 
     public const string RepoOwner = "iXendeRouS";
     public const string RepoName = "TerrainSwap";
